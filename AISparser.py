bfe4ec498ac7b3aaa4a36b72d8e6b6834c8bb4d7
unknown <BJiaShen@T062-300.dsonet.corp.root>
2013-07-26 10:09:49 +0800

    Turn URL2page_src into a function

--- AISparser.py
+++ AISparser.py
@@ -4,6 +4,14 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
+def get_src(URL):
+    """Returns the page source from a URL"""
+    #response = urllib2.urlopen(URL)
+    #src = response.read()
+    src = requests.get(URL).text
+
+    return src
+
 def is_number(s):
     """Checks if a string is a number"""
     try:
@@ -12,7 +20,7 @@ def is_number(s):
     except ValueError:
         return False
 
-def ship_src2data(src):
+def get_ship_data(src):
     """Returns ship data parsed from the source code of a ship page
     """
 
@@ -47,26 +55,40 @@ Draft (max):
 
     return boat_data
 
-root_URL = 'http://www.marinetraffic.com/ais/'
-all_ships_URL = 'http://www.marinetraffic.com/ais/datasheet.aspx?datasource=SHIPS_CURRENT&alpha=A&level0=200'
-URL = all_ships_URL
-src = requests.get(URL).text
-soup = BeautifulSoup(src)
-tags = soup.find_all(href=re.compile("shipdetails"))
-names = [tag.string for tag in tags]
-URLs = [root_URL + tag.get('href') for tag in tags]
+##     <a href="datasheet.aspx?datasource=SHIPS_CURRENT&amp;alpha=A&amp;level0=200&amp;orderby=SHIPNAME&amp;sort_order=ASC&amp;var_page=2">
+##      <img alt="Next page" border="0" height="11" src="icons/next.gif" width="9"/>
+##     </a>
 
-found = False
-for name, URL in zip(names, URLs):
-    is_PUSAN = name.find('PUSAN') != -1
-    if is_PUSAN:
-        found = True
-    elif not found:
-        continue
-    src = requests.get(URL).text
-    ship_data = ship_src2data(src)
-    print name, ship_data
-#URL = 'http://www.marinetraffic.com/ais/shipdetails.aspx?MMSI=897456478'
-#src = requests.get(URL).text
-#data = ship_src2data(src)
+#def main():
+if True:
+    root_URL = 'http://www.marinetraffic.com/ais/'
+    all_ships_URL = 'http://www.marinetraffic.com/ais/datasheet.aspx?datasource=SHIPS_CURRENT&alpha=A&level0=200'
+    URL = all_ships_URL
+    src = get_src(URL)
 
+    soup = BeautifulSoup(src)
+    tags = soup.find_all(href=re.compile("shipdetails"))
+
+    # Find the URL of the next page
+
+##    names = [tag.string for tag in tags]
+##    URLs = [root_URL + tag.get('href') for tag in tags]
+##
+##    found = False
+##    for name, URL in zip(names, URLs):
+####        is_PUSAN = name.find('PUSAN') != -1
+####        if is_PUSAN:
+####            found = True
+####        elif not found:
+####            continue
+##        if len(name) == 1:
+##            continue
+##        src = get_src(URL)
+##        ship_data = get_ship_data(src)
+##        print name, ship_data
+##
+##        with open(name, 'w') as fid:
+##            fid.write(src.encode('utf8'))
+##        with open(name, 'r') as fid:
+##            incoming_src = fid.read().decode('utf8')
+##        break
